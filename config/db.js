0d9843f398f12a4c4d25d17565d697162ad8e3fb
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-01 21:57:36 +0530

    chal jana bc

--- config/db.js
+++ config/db.js
@@ -1,33 +1,33 @@
-import pkg from "pg";
+// import pkg from "pg";
 import env from "dotenv";
 
-// import postgres from "postgres";
+import postgres from "postgres";
 
-// const connectionString = process.env.DATABASE_URL;
-// const db = postgres(connectionString);
-
-// export default db;
-
-const { Client } = pkg;
-env.config();
-
-const db = new Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  //   sslmode: "require",
-  // },
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-db.connect()
-  .then(() => console.log("✅ Connected to Supabase Postgres"))
-  .catch((err) => console.error("❌ DB connection error:", err));
+const connectionString = process.env.DATABASE_URL;
+const db = postgres(connectionString);
 
 export default db;
+
+// const { Client } = pkg;
+// env.config();
+
+// const db = new Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+//   ssl: {
+//     rejectUnauthorized: false,
+//     sslmode: "require",
+//   },
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// db.connect()
+//   .then(() => console.log("✅ Connected to Supabase Postgres"))
+//   .catch((err) => console.error("❌ DB connection error:", err));
+
+// export default db;
