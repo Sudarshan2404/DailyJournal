f66169f9fd280da84229de64ba81eecb1e9dfb36
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-01 21:45:29 +0530

    dont know what id did but did

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 import pkg from "pg";
 import env from "dotenv";
+
+// import postgres from "postgres";
+
+// const connectionString = process.env.DATABASE_URL;
+// const db = postgres(connectionString);
+
+// export default db;
+
 const { Client } = pkg;
 env.config();
 
@@ -9,12 +17,14 @@ const db = new Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-    sslmode: "require",
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  //   sslmode: "require",
+  // },
 });
 
-db.connect();
+db.connect()
+  .then(() => console.log("✅ Connected to Supabase Postgres"))
+  .catch((err) => console.error("❌ DB connection error:", err));
 
 export default db;
